53abf2fc509152de541df2c6a35fcf9cdea753a0
Dolly Tyagi <37585048+DollyTyagi@users.noreply.github.com>
2024-01-31 23:55:06 +0530

    First Question Rotate Array -commit1

---
+++ Lets-Play-with-DataStructure/src/arraysQuestions/RotateArray.java
@@ -0,0 +1,19 @@
+package arraysQuestions;
+
+public class RotateArray {
+	
+	int[] arr = { 2,3,4,5,6,7};
+
+	public static void main(String[] args) {
+		
+		RotateArray obj = new RotateArray();
+		
+
+	}
+	
+	public static void rotateArray(int[] arr) {
+		
+		
+	}
+
+}
